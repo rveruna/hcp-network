a98d1b26f902f91894a7806df305ff9d44ec8c9d
veronikar <veronika.rosicova@RD-VeronikaR2-Mac.local>
2025-06-16 21:37:01 +0100

    add node click handler to update selected HCP in Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import GraphCanvas from './components/GraphCanvas'
+import { useSelector } from 'react-redux'
+import type { RootState } from './store'
 
 function App() {
+  const selected = useSelector((state: RootState) => state.hcpGraph.selectedHCPId)
 
   return (
     <div>
       <h1>Healthcare Graph</h1>
       <GraphCanvas />
+      <p>Selected HCP ID: {selected ?? 'none'}</p>
     </div>
   )
 }
--- src/components/GraphCanvas.tsx
+++ src/components/GraphCanvas.tsx
@@ -1,7 +1,11 @@
 import ForceGraph2D from 'react-force-graph-2d'
 import { mockGraphData } from '../data/mockGraph'
+import { useDispatch } from 'react-redux'
+import { setSelectedHCPId } from '../features/hcpGraphSlice'
 
 function GraphCanvas() {
+  const dispatch = useDispatch()
+
   return (
     <div style={{ width: '100%', height: '600px' }}>
       <ForceGraph2D
@@ -11,6 +15,9 @@ function GraphCanvas() {
         linkLabel="label"
         linkDirectionalArrowLength={6}
         linkDirectionalArrowRelPos={1}
+        onNodeClick={(node: { id: string }) => {
+          dispatch(setSelectedHCPId(node.id))
+        }}
       />
     </div>
   )
